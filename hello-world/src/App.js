2ca0547f71fa5d83036ba0deb386286a111f532a
19mddilAspire <mddilshadul98@gmail.com>
2022-09-30 22:10:15 +0600

    src modification

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,26 @@
-
+import React from 'react';
 import './App.css';
+
+
+//JSX
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Person />
     </div>
   );
+  // return React.createElement('div', { className: "App" }, React.createElement('h1', null, 'Hello World'), <Person />);
+}
+
+function Person() {
+  return (
+    <div>
+      <h1>I am a person component</h1>
+    </div>
+  )
+  // return React.createElement('div', null, React.createElement('h1', null, "I am person component"));
 }
 
 export default App;
